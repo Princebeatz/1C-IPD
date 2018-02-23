1a441c0005ff997357c40329b1da444aec22fc6b
Princebeatz <36002492+Princebeatz@users.noreply.github.com>
2018-02-23 09:02:58 -0700

    team 1 final

--- team1.py
+++ team1.py
@@ -1,3 +1,4 @@
+import random
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -17,23 +18,19 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-    return [100]
-    def collude():
-        if collude:
             then[CCCCBbbbcccC]
-            '''Overall colluding happening the most'''
-            
-def player_1_betray():
-    '''if player_1 betrays, betray'''
-list[C,B,c,b]
-
-
-
-def C():
-    C='Colludes'
-if '''CCCCbbbCbB''':
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
+    if 'b' in their_history[-10:]:
+        return 'b'
+    elif random.random()<0.1:
+        return 'b'
+    
+    elif their_history[-1]== 'c':
+        return 'c'
+    elif random.random()<0.3:
+        return 'c'
+    else:
+        return 'c'
+  # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
     # The most recent round is my_history[-1] and their_history[-1].
@@ -41,7 +38,6 @@ if '''CCCCbbbCbB''':
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return ('c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
