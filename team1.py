67cc96a594a07f2cda39515b32dbd30e4ced802c
Princebeatz <36002492+Princebeatz@users.noreply.github.com>
2018-02-23 07:57:24 -0700

    20-36
    Added more code

--- team1.py
+++ team1.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'MF Strategy'
+strategy_description = 'This is for a grade so bring it on'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,6 +17,21 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
+    return [100]
+    def collude():
+        if collude:
+            then[CCCCBbbbcccC]
+            '''Overall colluding happening the most'''
+            
+def player_1_betray():
+    '''if player_1 betrays, betray'''
+list[C,B,c,b]
+
+
+
+def C():
+    C='Colludes'
+if '''CCCCbbbCbB''':
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -26,7 +41,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    return ('c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
@@ -53,7 +68,7 @@ if __name__ == '__main__':
               my_score=0,
               their_score=0,
               result='b'):
-         print 'Test passed'
+         print ('Test passed')
      # Test 2: Continue betraying if they collude despite being betrayed.
     test_move(my_history='bbb',
               their_history='ccc', 
